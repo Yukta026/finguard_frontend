b032169e53f843a39c8f3bf49a80fad246cf55d1
Yukta Gurnani <yuktagurnani.026@gmail.com>
2025-10-18 21:58:40 -0300

    refactor: add node: prefix to imports

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
